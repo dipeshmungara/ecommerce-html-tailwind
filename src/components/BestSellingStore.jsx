5e4d5e143e1fe866780ece52bf9cf862157d5144
stilala <savantilala27@gmail.com>
2024-05-09 15:48:00 +0530

    store section-working

--- src/components/BestSellingStore.jsx
+++ src/components/BestSellingStore.jsx
@@ -25,7 +25,7 @@ export default function BestSellingStore() {
                         </div>
 
                     </div>
-                    <div className="grid grid-cols-2 col-span-2">
+                    <div className="grid grid-cols-2 col-span-2 gap-8">
                         <div>{<NikeSaeMall />}</div>
                         <div>c</div>
                         <div>d</div>
--- src/components/NikeSaeMall.jsx
+++ src/components/NikeSaeMall.jsx
@@ -1,15 +1,17 @@
 import cap from "../images/cap.jpg";
 import { LuCrown } from "react-icons/lu";
+import handbag from "../images/handbag.jpg"
+import Tshirt from "../images/Isolated_white_t-shirt_front.jpg";
 
 
 export default function NikeSaeMall() {
     return (
         <>
 
-            <div>
+            <div className="border border-gray-200 rounded-2xl shadow dark:bg-gray-800 dark:border-gray-700 p-5">
                 <div className="flex items-center">
                     <div className="relative">
-                        <img src={cap} alt="" className="w-full max-w-[80px] h-[80px] rounded-full" />
+                        <img src={cap} alt="" className="w-full max-w-[80px] h-[80px] rounded-full object-cover" />
                         <div class="absolute top-[40px] inline-flex items-center justify-center w-8 h-8 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
                             <LuCrown  className="text-[20px]"/>
                         </div>
@@ -18,6 +20,26 @@ export default function NikeSaeMall() {
                         <h3 className="text-black font-bold text-[24px]">Nike Sae Mall</h3>
                         <p className="text-gray-500 text-[18px] font-medium">
                             "Just do it bro!"
+                        </p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 mt-5 gap-4">
+                    <div className="">
+                        <img src={handbag} className="w-full h-[130px] rounded-xl object-cover" alt="" />
+                        <p className="font-medium mt-2 text-[18px] text-center">
+                            Rp650.000
+                        </p>
+                    </div>
+                    <div>
+                        <img src={cap} className="w-full h-[130px] rounded-xl object-cover" alt="" />
+                        <p className="font-medium mt-2 text-[18px] text-center">
+                            Rp99.000
+                        </p>
+                    </div>
+                    <div>
+                        <img src={Tshirt} className="w-full h-[130px] rounded-xl object-cover" alt="" />
+                        <p className="font-medium mt-2 text-[18px] text-center">
+                            Rp270.000
                         </p>
                     </div>
                 </div>
